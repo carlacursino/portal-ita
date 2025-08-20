a1fadf361bee96e66286b5ebd57e6177f4defe3b
Carla Cursino <cursino@ita.br>
2025-08-20 12:02:53 -0300

    mudar e-mail de autoria do site drone

--- site01/translations/drone/pt.js
+++ site01/translations/drone/pt.js
@@ -3,5 +3,8 @@ module.exports = {
         initiatives: {
             _title: 'Patrocinadores',
         },
+        credits: {
+            author: "Criação: <a href='mailto:drone@ita.br'>drone@ita.br</a>",
+        },
     },
 }
