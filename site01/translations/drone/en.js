849e0ea814b8e304cacd6204d6d5a48cc6703b5f
Carla Cursino <cursino@ita.br>
2025-09-04 12:10:03 -0300

    destaques

--- site01/translations/drone/en.js
+++ site01/translations/drone/en.js
@@ -6,5 +6,8 @@ module.exports = {
         credits: {
             author: "Creation: <a href='mailto:drone@ita.br'>drone@ita.br</a>",
         },
+        destaques: {
+            _title: 'Research Fields'
+        },
     },
 }
--- site01/translations/drone/pt.js
+++ site01/translations/drone/pt.js
@@ -6,5 +6,8 @@ module.exports = {
         credits: {
             author: "Criação: <a href='mailto:drone@ita.br'>drone@ita.br</a>",
         },
+        destaques: {
+            _title: 'Áreas de Pesquisa',
+        },
     },
 }
