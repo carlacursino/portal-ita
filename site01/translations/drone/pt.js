c69073bdcee7da272f56346921674d66d3c395f0
Carla Cursino <cursino@ita.br>
2025-09-05 14:00:36 -0300

    research fields

--- site01/translations/drone/pt.js
+++ site01/translations/drone/pt.js
@@ -7,7 +7,7 @@ module.exports = {
             author: "Criação: <a href='mailto:drone@ita.br'>drone@ita.br</a>",
         },
         destaques: {
-            _title: 'Áreas de Pesquisa',
+            _title: 'Campos de Pesquisa',
         },
     },
 }
