c14a5a858e3424f1f23a8ba89a7d2701589f80b5
Carla Cursino <cursino@ita.br>
2025-09-05 14:28:50 -0300

    add publications

---
+++ site01/models/Publication.js
@@ -0,0 +1,33 @@
+var
+    capstone = require('capstonejs'),
+    config = require('config'),
+    translations = require('capstone-intl'),
+    fileManager = require('capstone-file-manager'),
+    Types = capstone.Field.Types,
+    Publication = new capstone.List('Publication', {
+        map: { name: 'title' },
+        track: true,
+        autokey: { path: 'slug', from: 'title.pt', unique: true },
+        defaultSort: '-publishedDate',
+    })
+
+new fileManager(Publication).init()
+
+Publication.add({
+    title: { type: String, required: true, intl: true },
+    authors: { type: Types.Relationship, ref: 'Post', many: true, required: true },
+    abstract: { type: Types.Html, wysiwyg: true, height: 150, intl: true },
+    project: { type: String },
+    cite: { type: String },
+    doi: { type: Types.Url },
+    link: { type: Types.Url },
+    dataset: { type: Types.Url },
+    video: { type: Types.Url },
+    file: { type: Types.File },
+})
+
+Publication.schema.plugin(translations, { languages: config.cms['supported languages'], defaultLanguage: config.cms.language })
+
+Publication.defaultColumns = 'title'
+
+Publication.register()
