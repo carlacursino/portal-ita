442de9d66fd7affb94a10f340dcc8b9c62017c8b
Carla Cursino <cursino@ita.br>
2025-09-06 09:11:04 -0300

    add compose bake

--- site01/scripts/container.drone.sh
+++ site01/scripts/container.drone.sh
@@ -7,5 +7,5 @@ docker rmi drone || true
 
 # set "DOCKER_BUILDKIT=1" && docker build -t drone:latest --ssh default=$HOME/.ssh/id_ed25519 .
 # docker run -d --network=netlab01 --name drone drone:latest
-set "DOCKER_BUILDKIT=1" && set USER_ID=$(id -u) && set GROUP_ID=$(id -g) && docker compose -f docker-compose-drone.yaml up --build --detach
+set "COMPOSE_BAKE=true" && set "DOCKER_BUILDKIT=1" && set "USER_ID=$(id -u)" && set "GROUP_ID=$(id -g)" && docker compose -f docker-compose-drone.yaml up --build --detach
 # docker logs drone --follow
