99b57e0454fd67e4defa390477426ea505325b18
Carla Cursino <cursino@ita.br>
2025-09-07 17:00:30 -0300

    refactor build

--- site01/scripts/container.ceds.sh
+++ site01/scripts/container.ceds.sh
@@ -5,4 +5,4 @@ docker stop ceds || true
 docker rm ceds || true
 docker rmi ceds || true
 
-DOCKER_BUILDKIT=1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose -f docker-compose-drone.yaml up --build --detach
+DOCKER_BUILDKIT=1 ECOSYSTEM=ceds PORT=3002 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose -f docker-compose-drone.yaml up --build --detach
--- site01/scripts/container.pgcomp.sh
+++ site01/scripts/container.pgcomp.sh
@@ -5,4 +5,4 @@ docker stop pgcomp || true
 docker rm pgcomp || true
 docker rmi pgcomp || true
 
-DOCKER_BUILDKIT=1 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose -f docker-compose-drone.yaml up --build --detach
+DOCKER_BUILDKIT=1 ECOSYSTEM=pgcomp PORT=3004 USER_ID=$(id -u) GROUP_ID=$(id -g) docker compose -f docker-compose-drone.yaml up --build --detach
