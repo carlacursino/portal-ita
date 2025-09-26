cc089fafcb2afac9f47cef037973ebe6d6f7c661
Cursino, C <cursino@ita.br>
2025-09-26 12:22:54 -0300

    config smtp

--- site01/helpers/config.js
+++ site01/helpers/config.js
@@ -9,8 +9,15 @@ const
     validation = require('../'.concat(defaults.cms['validator rules']))
     editor = require('../'.concat(defaults.cms['editor rules']))
     headers = require('../'.concat(defaults.cms['headers rules']))
+    smtp = {
+        smtp: {
+            user: process.env.SMTPUSR,
+            password: process.env.SMTPPWD,
+            server: process.env.SMTPSRV
+        }
+    }
     custom = require('../config/custom/'.concat(env).concat('.js'))
-    config = _.merge({}, defaults, access, validation, editor, headers, custom)
+    config = _.merge({}, defaults, access, validation, editor, headers, smtp, custom)
 
 config.env = env
 
