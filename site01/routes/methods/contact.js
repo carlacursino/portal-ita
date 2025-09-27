abd1b8557253506ec8205e11b43392bd91b7e5c7
Cursino, C <cursino@ita.br>
2025-09-27 14:14:29 -0300

    formatting

--- site01/routes/methods/contact.js
+++ site01/routes/methods/contact.js
@@ -1,42 +1,39 @@
-const Email = require('capstone-email');
-const config = require('config');
+const 
+    Email = require('capstone-email')
+    config = require('config')
 
 module.exports = (req, res) => {
-  const { name, email, message } = req.body;
+    const { name, email, message } = req.body
 
-  if (!name || !email || !message) {
-    return res.status(400).send('Missing required fields');
-  }
-
-  options = {
-    host: config.smtp.server,
-    port: config.smtp.port,
-    secure: false,
-    auth: {
-      user: config.smtp.user,
-      pass: config.smtp.password,
-    },
-  }
-
-  new Email('emails/contact.hbs', {
-    transport: 'nodemailer',
-    engine: config.cms['custom engine'],
-    root: 'templates',
-  }).send({
-    name,
-    email,
-    message,
-  }, {
-    from: config.portal.contato.contatos.mailProjeto,
-    to: { email: email, name: name },
-    nodemailerConfig: options,
-  },
-    (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error sending email');
-      }
-      return res.status(200).send('Email sent successfully');
+    if (!name || !email || !message) {
+        return res.status(400).send('Missing required fields')
     }
-  );
-};
+
+    options = {
+        host: config.smtp.server,
+        port: config.smtp.port,
+        secure: false,
+        auth: {
+            user: config.smtp.user,
+            pass: config.smtp.password,
+        },
+    }
+
+    new Email('contact.handlebars', {
+        transport: 'nodemailer',
+        engine: config.cms['custom engine'],
+        root: 'templates',
+    }).send({
+        name,
+        email,
+        message,
+    }, {
+        from: config.portal.contato.contatos.mailProjeto,
+        to: { email: email, name: name },
+        nodemailerConfig: options,
+    },
+    (err) => {
+        if(err)
+            console.log(err)
+    })
+}
