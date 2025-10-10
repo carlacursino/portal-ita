3ad2bdef8ec68de9851d50d941eece56d16450d3
Gomes, J.M <gomesjm@ita.br>
2025-10-10 11:47:32 -0300

    fix onoff backup config

--- site01/scripts/backup.sh
+++ site01/scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --env-file ./.backup-on-off.env -v portaldbdata:/backup/mongobackup:ro -v portaldata:/backup/portalbackup:ro -v portalcerts:/backup/certbotcerts:ro -v portalchallenges:/backup/certbotchallenges:ro -v /var/run/docker.sock:/var/run/docker.sock:ro offen/docker-volume-backup:latest
+docker run --rm -v portaldbdata:/backup/mongobackup:ro -v portaldata:/backup/portalbackup:ro -v portalcerts:/backup/certbotcerts:ro -v portalchallenges:/backup/certbotchallenges:ro -v /var/run/docker.sock:/var/run/docker.sock:ro offen/docker-volume-backup:latest
