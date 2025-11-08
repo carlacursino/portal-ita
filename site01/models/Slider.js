bb21aea6b0e3090be32ea4340ce52708322266e4
Carla Cursino <cursino@ita.br>
2025-11-08 11:48:58 -0300

    jump para ponto de página e ajuste vertical do cabeçalho

--- site01/models/Slider.js
+++ site01/models/Slider.js
@@ -2,17 +2,23 @@ var
     capstone = require('capstonejs'),
     config = require('config'),
     translations = require('capstone-intl'),
+    fileManager = require('capstone-file-manager'),
     Types = capstone.Field.Types,
     Slider = new capstone.List('Slider', {
         track: true,
         defaultSort: 'sequence',
     })
 
+new fileManager(Slider).init()
+
 Slider.add({
     sequence: { type: Types.Number, required: true, default: 0 },
     post: { type: Types.Relationship, required: true, initial: true, ref: 'Post', many: false },
     subTitle: { type: String, intl: true },
     subTitleAlignment: { type: Types.Select, options: 'left, center, right', default: 'center' },
+    route: { type: String },
+    link: { type: Types.Url },
+    image: { type: Types.File },
 })
 
 Slider.schema.plugin(translations, { languages: config.cms['supported languages'], defaultLanguage: config.cms.language })
