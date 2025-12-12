37635f0f00d23c75e9937be3e2e7b536e3881082
Cursino, C. <cursino@ita.br>
2025-12-12 22:41:29 +0000

    .

--- site01/helpers/gdrive.js
+++ site01/helpers/gdrive.js
@@ -1,37 +1,39 @@
-const 
+const
     config = require('config')
     axios = require('axios')
 
 const { google } = require('googleapis')
 
+const auth = new google.auth.GoogleAuth({
+    keyFile: config.embed.google.keyFile,
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+})
+
 async function getAccessToken() {
-    const 
-        scopes = ["https://www.googleapis.com/auth/drive.readonly"]
-        auth = new google.auth.GoogleAuth({
-            keyFile: config.embed.google.keyFile,
-            scopes: scopes,
-        })
-        client = await auth.getClient();
-        tokens = await client.getAccessToken();
+    const client = await auth.getClient();
+    const tokens = await client.getAccessToken();
 
     return tokens.token;
 }
 
 exports.list = async (folderId) => {
-  const token = await getAccessToken();
-
-  const url = "https://www.googleapis.com/drive/v3/files";
-  const params = {
-    q: `'${folderId}' in parents`,
-    fields: "files(id, name, mimeType, modifiedTime, size)"
-  }
-
-  const resp = await axios.get(url, {
-    params,
-    headers: {
-      Authorization: `Bearer ${token}`
+    const token = await getAccessToken()
+    const params = {
+        q: `'${folderId}' in parents and trashed = false`,
+        fields: "files(id, name, mimeType, modifiedTime, size)"
     }
-  })
-
-  return resp.data.files
+    const resp = await axios
+        .get(
+            "https://www.googleapis.com/drive/v3/files", {
+                params,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        )
+        .catch(err => {
+            throw err
+        })
+    
+    return resp.data.files
 }
