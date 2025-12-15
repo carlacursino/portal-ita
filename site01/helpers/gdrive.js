315f8b56dd6afc7415bf58080bb5147ae7e2f4bc
Cursino, C. <cursino@ita.br>
2025-12-15 17:48:29 +0000

    fix (google não config)

--- site01/helpers/gdrive.js
+++ site01/helpers/gdrive.js
@@ -4,16 +4,22 @@ const
 
 const { google } = require('googleapis')
 
-const auth = new google.auth.GoogleAuth({
-    keyFile: config.embed.google.keyFile,
-    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
-})
+let auth = null
+if (config.embed)
+    auth = new google.auth.GoogleAuth({
+        keyFile: config.embed.google.keyFile,
+        scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+    })
 
 async function getAccessToken() {
-    const client = await auth.getClient();
-    const tokens = await client.getAccessToken();
+    if (auth) {
+        const client = await auth.getClient()
+        const tokens = await client.getAccessToken()
 
-    return tokens.token;
+        return tokens.token
+    }
+    else 
+        return null
 }
 
 function getId(url) {
@@ -32,39 +38,43 @@ function getId(url) {
 exports.list = async (url) => {
     const folderId = getId(url)
     const token = await getAccessToken()
-    const params = {
-        q: `'${folderId}' in parents and trashed = false`,
-        fields: "files(id, name, mimeType, modifiedTime, size, webContentLink, webViewLink, iconLink)"
-    }
-    const resp = await axios
-        .get(
-            "https://www.googleapis.com/drive/v3/files", {
-                params,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }
-        )
-        .catch(err => {
-            throw err
-        })
-
-    const withLinks = resp.data.files.map(item => {
-        let action
-        const IS_FOLDER = item.mimeType === 'application/vnd.google-apps.folder'
-        if (IS_FOLDER)
-            action = item.webViewLink || `https://drive.google.com/drive/folders/${item.id}`
-        else if (item.webContentLink)
-            action = item.webContentLink;
-        else
-            action = `https://drive.google.com/uc?export=download&id=${item.id}`;
-        return {
-            ...item,
-            iconUrl: item.iconLink,
-            downloadLink: action,
-            isFolder: IS_FOLDER
+    if (token) {
+        const params = {
+            q: `'${folderId}' in parents and trashed = false`,
+            fields: "files(id, name, mimeType, modifiedTime, size, webContentLink, webViewLink, iconLink)"
         }
-    });
-    
-    return withLinks
+        const resp = await axios
+            .get(
+                "https://www.googleapis.com/drive/v3/files", {
+                    params,
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            )
+            .catch(err => {
+                throw err
+            })
+
+        const withLinks = resp.data.files.map(item => {
+            let action
+            const IS_FOLDER = item.mimeType === 'application/vnd.google-apps.folder'
+            if (IS_FOLDER)
+                action = item.webViewLink || `https://drive.google.com/drive/folders/${item.id}`
+            else if (item.webContentLink)
+                action = item.webContentLink;
+            else
+                action = `https://drive.google.com/uc?export=download&id=${item.id}`;
+            return {
+                ...item,
+                iconUrl: item.iconLink,
+                downloadLink: action,
+                isFolder: IS_FOLDER
+            }
+        })
+        
+        return withLinks
+    }
+    else
+        return null
 }
