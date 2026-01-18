3e42be3d84bfba79c43d830572fcd7423b49ce5e
Cursino, C. <cursino@ita.br>
2026-01-18 13:22:49 -0300

    direnv

--- .bin/build.sh
+++ .bin/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-find . -name "node_modules" -type d -prune -exec rm -rf {} + && rm -f package-lock.json && npm i --legacy-peer-deps && npm run build && npm audit fix --legacy-peer-deps
+find . -name "node_modules" -type d -prune -exec rm -rf {} + && \
+rm -f package-lock.json && \
+npm i --legacy-peer-deps && \
+npm run build && \
+npm prune --production --legacy-peer-deps && \
+npm audit fix --omit=dev --legacy-peer-deps
