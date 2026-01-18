7da0fc98035c4e78202587aeb16b62a483783cdb
Cursino, C. <cursino@ita.br>
2026-01-18 18:22:43 -0300

    add dep dir

--- .bin/build.sh
+++ .bin/build.sh
@@ -5,5 +5,5 @@ find . -name "node_modules" -type d -prune -exec rm -rf {} + && \
 rm -f package-lock.json && \
 npm i --legacy-peer-deps && \
 npm run build && \
-npm prune --production --legacy-peer-deps && \
+npm prune --omit=dev --legacy-peer-deps && \
 npm audit fix --omit=dev --legacy-peer-deps
