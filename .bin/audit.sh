42cb92e0397f9075cf310cac11f810bec06b2f66
Cursino, C. <cursino@ita.br>
2026-01-18 18:02:58 -0300

    fix sub module ref

--- .bin/audit.sh
+++ .bin/audit.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ "$*" != *"--low"* ]]; then
+if [[ "$*" == "--low" ]]; then
+    echo "Audit **LOW**"
     npm audit --severity=low --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "low") | select(.fixAvailable | type == "object")' > audit.json
 fi
 
-if [[ "$*" != *"--moderate"* ]]; then
+if [[ "$*" == "--moderate" ]]; then
+    echo "Audit **MODERATE**"
     npm audit --severity=moderate --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "moderate") | select(.fixAvailable | type == "object")' > audit.json
 fi
 
-if [[ "$*" != *"--high"* ]]; then
+if [[ "$*" == "--high" ]]; then
+    echo "Audit **HIGH**"
     npm audit --severity=high --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "high") | select(.fixAvailable | type == "object")' > audit.json
 fi
 
-if [[ "$*" != *"--critical"* ]]; then
+if [[ "$*" == "--critical" ]]; then
+    echo "Audit **CRITICAL**"
     npm audit --severity=critical --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "critical") | select(.fixAvailable | type == "object")' > audit.json
 fi
