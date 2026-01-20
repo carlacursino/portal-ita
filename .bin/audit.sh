3bde56ce5248f0b9fdef92198eaea689160007c3
Cursino, C <cursino@ita.br>
2026-01-20 11:37:19 -0300

    audit all

--- .bin/audit.sh
+++ .bin/audit.sh
@@ -1,29 +1,54 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ "$*" == "--low" ]]; then
+function low() {
     echo "👉 Audit **LOW**"
-    npm audit --severity=low --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "low") | select(.fixAvailable | type == "object")' > .audit.low.json
+    (npm audit --severity=low --json --legacy-peer-deps || true) | jq '.vulnerabilities[] | select(.severity == "low") | select(.fixAvailable | type == "object")' > .audit.low.json
+}
+
+function moderate() {
+    echo "👆 Audit **MODERATE**"
+    (npm audit --severity=moderate --json --legacy-peer-deps || true) | jq '.vulnerabilities[] | select(.severity == "moderate") | select(.fixAvailable | type == "object")' > .audit.moderate.json
+}
+
+function high() {
+    echo "🧨 Audit **HIGH**"
+    (npm audit --severity=high --json --legacy-peer-deps || true) | jq '.vulnerabilities[] | select(.severity == "high") | select(.fixAvailable | type == "object")' > .audit.high.json
+}
+
+function critical() {
+    echo "💣 Audit **CRITICAL**"
+    (npm audit --severity=critical --json --legacy-peer-deps || true) | jq '.vulnerabilities[] | select(.severity == "critical") | select(.fixAvailable | type == "object")' > .audit.critical.json
+}
+
+if [[ "$*" == "--low" ]]; then
+    low
     exit 0
 fi
 
 if [[ "$*" == "--moderate" ]]; then
-    echo "👆 Audit **MODERATE**"
-    npm audit --severity=moderate --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "moderate") | select(.fixAvailable | type == "object")' > .audit.moderate.json
+    moderate
     exit 0
 fi
 
 if [[ "$*" == "--high" ]]; then
-    echo "🧨 Audit **HIGH**"
-    npm audit --severity=high --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "high") | select(.fixAvailable | type == "object")' > .audit.high.json
+    high
     exit 0
 fi
 
 if [[ "$*" == "--critical" ]]; then
-    echo "💣 Audit **CRITICAL**"
-    npm audit --severity=critical --json --legacy-peer-deps | jq '.vulnerabilities[] | select(.severity == "critical") | select(.fixAvailable | type == "object")' > .audit.critical.json
+    critical
     exit 0
 fi
 
-echo "👎 Invalid option `$*`"
+if [[ "$*" == "--all" ]]; then
+    echo "🌐 Audit **ALLLOW**"
+    low
+    moderate
+    high
+    critical
+    exit 0
+fi
+
+echo " Invalid option `$*`"
 exit 1
